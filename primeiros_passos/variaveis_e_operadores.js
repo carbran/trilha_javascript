f349dc114f4a9ff74ba801d126edf4bc779f9eb5
Fernando Carlos Brandão Filho <brandao.fernando96@gmail.com>
2025-10-14 22:42:45 -0300

    :construction: primeiras aulas de js, variaveis, operadores e condicionais

---
+++ primeiros_passos/variaveis_e_operadores.js
@@ -0,0 +1,17 @@
+// Declaração de variáveis
+let nome = "João"; // Pode ser alterado
+const idade = 25; // Não pode ser alterado
+
+nome = "Fernando";
+
+// console.log(nome);
+
+//idade = 30; // erro
+
+const num1 = 5.79;
+const num2 = 1240;
+
+const resultado = num2 / num1;
+
+console.log(resultado.toFixed(2)); // toFixed arrendonda e transforma em string
+
